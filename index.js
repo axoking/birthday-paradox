b9b8200bc97ff45963ea2be68accd1b3472dfca4
axoking <110609189+axoking@users.noreply.github.com>
2026-04-02 18:49:10 +0000

    bruh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getRandomBirthday() {
 }
 
 function birthdayToString(dayOfYear) {
-    // input: number 0-355
+    // input: number 0-364
     // output: e.g. "1. Jan"
     const months = [
         ["Jan", 31],
@@ -19,7 +19,7 @@ function birthdayToString(dayOfYear) {
         ["Nov", 30],
         ["Dez", 31]
     ];
-    var day = dayOfYear;
+    var day = dayOfYear + 1;
     var monthIndex;
     for (monthIndex = 0; monthIndex < 12; monthIndex++) {
         const [name, daysThisMonth] = months[monthIndex];
@@ -31,8 +31,8 @@ function birthdayToString(dayOfYear) {
 }
 
 function findPairBirthdays(birthdays) {
-    // input: list of numbers 0-355
-    // output: list of numbers 0-355
+    // input: list of numbers 0-364
+    // output: list of numbers 0-364
     var result = [];
     for (const day of birthdays) {
         if (birthdays.includes(day) && !result.includes(day)) {
@@ -42,4 +42,54 @@ function findPairBirthdays(birthdays) {
     return result;
 }
 
-function 
+function getRandomBirthdayArray(count) {
+    const array = [];
+    for (var i = 0; i < count; i++)
+        array.push(getRandomBirthday());
+    return array;
+}
+
+function updatePeople(change) {
+    // change: number (+1 or -1)
+    const newCount = peopleCount + change;
+    if (newCount < 2 || newCount > 100) return;
+    peopleCount = newCount;
+
+    tries = 0;
+    succeses = 0;
+
+    const [birthdays, pairs] = calculate();
+    display(birthdays, pairs);
+}
+
+function calculate() {
+    // generates random birthday for every person, finds pairs and updates the statistics
+    // returns the birthdays and pairs
+    const birthdays = getRandomBirthdayArray(peopleCount);
+    const pairs = findPairBirthdays(birthdays);
+    tries++;
+    if (pairs.length) succeses++;
+    return [birthdays, pairs];
+}
+
+function display(birtdays, pairs) {
+    // both inputs: list of numbers 0-364
+    const peopleElements = birthdays.map(birthday => {
+        const element = document.createElement("div");
+        element.classList.add("person");
+        element.innerText = birthdayToString(birthday);
+        if (pairs.includes(birthday))
+            element.classList.add("success");
+        return element;
+    });
+    peopleTag.innerHTML = "";
+    for (const element of peopleElements) {
+        peopleTag.appendChild(element);
+    }
+}
+
+const peopleTag = document.getElementById("people");
+
+var peopleCount = 2;
+var tries = 0;
+var succeses = 0;
